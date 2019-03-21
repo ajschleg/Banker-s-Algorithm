8e1e9ee66f739d466f9ec0d1e834e8a084e66ea1
Austin <schlegelas@outlook.com>
2019-03-21 18:00:22 -0400

    Working on release resources.

--- relResources.c
+++ relResources.c
@@ -1,6 +1,26 @@
+#include <stdio.h>
 #include "main.h"
 
 int release_resources(int customer_num, int release[])
 {
-	return customer_num;
+    int difference[NUMBER_OF_RESOURCES];
+	/* Release resources should release from allocation array and move those resources to available array*/
+    for (int i = 0; i < NUMBER_OF_RESOURCES; ++i)
+    {
+        /* Calculate the difference of allocation and release */
+        difference[i] = allocation[customer_num][i] - release[i];
+
+        /* Check if able to release those resources*/
+        if(difference[i] < 0)
+        {
+            printf("Not able to release\n");
+            return -1;
+        }
+
+        /* Calc new allocation after determining we can release*/
+        allocation[customer_num][i] -= release[i];
+
+        /* Add the difference to available */
+
+    }
 }
